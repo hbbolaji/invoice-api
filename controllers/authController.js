79c9522d260311bacc4901d151bbb941b7a9e5a9
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2024-01-16 15:54:02 +0300

    login feature added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,12 @@
 const jwt = require("jsonwebtoken");
 const User = require("./../models/userModels");
+const { MongoError } = require("mongodb");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET, {
+    expiresIn: process.env.EXPIRES_IN,
+  });
+};
 
 exports.signup = async (req, res, next) => {
   try {
@@ -10,18 +17,37 @@ exports.signup = async (req, res, next) => {
       password: req.body.password,
       passwordConfirm: req.body.passwordConfirm,
     });
-    const token = jwt.sign(
-      {
-        id: newUser._id,
-      },
-      process.env.SECRET,
-      { expiresIn: process.env.EXPIRES_IN }
-    );
-
+    const token = signToken(newUser._id);
     res.status(201).json({
       status: "success",
       token: `Bearer ${token}`,
       data: newUser,
     });
-  } catch (error) {}
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    // check if there is email and password
+    if (!email || !password) {
+      return next(new Error("please provide email and password"));
+    }
+    // check if user exist
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.correctPassword(password, user.password)))
+      return next(new Error("Incorrect Email or Password"));
+    // generate token
+    const token = signToken(user._id);
+    // send token
+    res.status(200).json({
+      message: "success",
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
