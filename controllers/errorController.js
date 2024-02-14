1a7b36dc712efafa08332bd144a075aa5843ee82
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2024-02-14 17:13:22 +0300

    production and development error messages seperated

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,33 @@
+const sendErrorDev = (res, error) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    error,
+    message: error.message,
+    stack: error.stack,
+  });
+};
+
+const sendErrorProd = (res, error) => {
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    console.error("Error 💥", error);
+    res.status(500).json({
+      status: "error",
+      message: "something went wrong",
+    });
+  }
+};
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || "error";
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(res, error);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(res, error);
+  }
 };
